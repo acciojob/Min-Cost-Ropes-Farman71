e69c717b192d74fa1e6b53036fb59436202efa69
Farman71 <farmanhussain71@gmail.com>
2025-01-15 19:03:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+let sum=0;
+let arrSort = arr.sort((a, b) => a - b);  // Sorting in ascending order
+for (let i = 0; i < arrSort.length - 1; i++) {
+  let result = arrSort[i] + arrSort[i + 1];
+  arrSort.splice(i, 2);
+  arrSort.push(result);
+  arrSort.sort((a, b) => a - b);
+  i--;
+  sum=sum+result
+}
+return sum
   
 }
 
